fa26fe3e7c53064d1f6e4d9593c2ec56c8c2a66d
andreapeterson <andrea.peterson432@gmail.com>
2023-12-09 00:31:40 -0500

    cicd added

--- terra/providers.tf
+++ terra/providers.tf
@@ -5,6 +5,11 @@ terraform {
       version = "5.26.0"
     }
   }
+  backend "s3" {
+    bucket                  = "terra444"
+    key                     = "my-terraform-project"
+    region                  = "us-east-1"
+  }
 }
 
 provider "aws" {
