5efa7174179fa8825aaad7b8a1aa4ee7191b4048
andreapeterson <andrea.peterson432@gmail.com>
2023-12-11 01:33:14 -0500

    Lambda Function update

--- terra/lambda_function.py
+++ terra/lambda_function.py
@@ -5,34 +5,33 @@ table_name = os.environ['table_name']
 
 client = boto3.client('dynamodb')
 
-count = 0
-
 def lambda_handler(event, context):
-    global count
-    if count == 0:
-        client.put_item(
-            TableName=table_name,
-            Item={
-                'id': {
-                    'S': '1'},
-                'Views': {
-                    'N': '1'}
-            })
-        count += 1
-        return 1
-    else:
-        response2 = client.update_item(
+    try:
+        response = client.get_item(
             TableName=table_name,
             Key={
-                'id': {
-                    'S': '1'}
-            },
-            ExpressionAttributeNames={'#V': 'Views'},
-            ExpressionAttributeValues={':v': {'N': '1'}},
-            UpdateExpression='SET #V = #V + :v',
-            ReturnValues='ALL_NEW'
+                'id': {'S': '1'}
+            }
         )
+        if 'Item' not in response:
+            client.put_item(
+                TableName=table_name,
+                Item={
+                    'id': {'S': '1'},
+                    'Views': {'N': '1'}
+                }
+            )
+            num_views = 1
+        else:
+            response = client.update_item(
+                TableName=table_name,
+                Key={'id': {'S': '1'}},
+                ExpressionAttributeNames={'#V': 'Views'},
+                ExpressionAttributeValues={':v': {'N': '1'}},
+                UpdateExpression='SET #V = #V + :v',
+                ReturnValues='ALL_NEW'
+            )
 
-        num_views = response2['Attributes']['Views']['N']
+            num_views = int(response['Attributes']['Views']['N'])
 
         return num_views
